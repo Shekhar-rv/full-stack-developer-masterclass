c6d581310846d618c554dd5f7b9d477bf6a8925e
Shekhar-rv <shekhar.rv@gmail.com>
2024-05-20 11:23:14 +0000

    * Created new project and app that will used for the reminder of the course. * Removed old project and app.

--- src/course/python/django/my_site/my_site/urls.py
+++ src/course/python/django/my_site/my_site/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for first_project project.
+URL configuration for my_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('first_app/', include('first_app.urls')), # first_project/urls.py
     path('admin/', admin.site.urls),
 ]
