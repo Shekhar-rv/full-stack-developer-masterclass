2773f2ec67a09f03a6a7ea91d913226cdbbbef2e
Shekhar-rv <shekhar.rv@gmail.com>
2024-05-22 15:59:53 +0000

    * Completed For loops. * Control statements. * Tags and URL names in template.

--- src/course/python/django/my_site/my_app/views.py
+++ src/course/python/django/my_site/my_app/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
 
-def example_view(request):
+def example_view(request: HttpRequest):
+    '''
+    This is a view function that returns a simple HTML page.
+    '''
     return render(request, 'my_app/example.html')
 
-def variable_view(request):
-    my_var = {
-        'first_name': 'John',
-        'last_name': 'Doe',
-        'some_list': [1, 2, 3],
-        'some_dict': {'key1': 'value1'},
-    }
-    return render(request, 'my_app/variable.html', context=my_var)
+# def variable_view(request: HttpRequest):
+#     '''
+#     This is a view function that returns a simple HTML page with variables.
+#     '''
+#     my_var = {
+#         'first_name': 'john',
+#         'last_name': 'doe',
+#         'some_list': [1, 2, 3],
+#         'user_logged_in': True,
+#     }
+#     return render(request, 'my_app/variable.html', context=my_var)
+
+def variable_view(request: HttpRequest):
+    '''
+    This is a view function that returns a simple HTML page with variables.
+    '''
+
+    return render(request, 'my_app/variable.html')
