bdf1c3d035058763836bf6b9760c289e2218140e
Shekhar-rv <shekhar.rv@gmail.com>
2024-05-22 11:18:17 +0000

    * Completed Views, Routing and URLs

--- src/course/python/django/my_site/first_app/urls.py
+++ src/course/python/django/my_site/first_app/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 # first_app/
 urlpatterns = [
-    path('<int:num_page>', views.num_page_view),
-    path('<str:topic>/', views.news_view),
-    path('<int:num1>/<int:num2>/', views.add_view),
+    path('', views.simple_view) # domain.com/first_app/
 ]
