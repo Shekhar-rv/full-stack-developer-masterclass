05bbf521997287d72abf991915230310ea857169
Shekhar-rv <shekhar.rv@gmail.com>
2024-05-23 16:22:32 +0000

    Completed Models, Databases and Queries * Filter and get. * Field Look ups. * Updating models and entries and deleting entries.

--- src/course/python/django/my_dental_site/office/views.py
+++ src/course/python/django/my_dental_site/office/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .models import Patient
 
 # Create your views here.
+def list_patients(request: HttpRequest):
+    all_patients = Patient.objects.all()
+    return render(request, 'office/list.html', context={'patients': all_patients})
